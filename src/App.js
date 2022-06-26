9d3ac6a97bf7c1b0aff96f259404b9fff551cc74
amishasubedi <amisha.subedi1@gmail.com>
2022-06-26 17:41:57 -0500

    component updates in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
+// React will only rerun App function if state changes
+// only the difference between virtual snapshots is considered for the updates made through the real DOM.
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false); // showing element conditionally
+  // so currently our state is false, so we only see Hi there. - add function to change the state
+
+  console.log("App Running"); // since app executes every time state changes, App running will be output on console everytime you click toggle
+
+  const toggleParagraphHandler = () => {
+    // opposite of show paragraph, but instead of passing it directly,
+    // use a function, pass it as a parameter then reverse it
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+
+      {/* this function triggers Toggle Paragraph Button when clicked*/}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
